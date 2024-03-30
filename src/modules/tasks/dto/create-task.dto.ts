75e5dfa0f6a9698935bfaae9e19be2804a65c701
ukswift <utkarshkousik@gmail.com>
2024-03-30 13:03:42 +0530

    Setup global validation

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  summary: string;
+}
