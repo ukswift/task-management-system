dbc74bb97308be3d90c7cf23abc060bf14bdda4f
ukswift <utkarshkousik@gmail.com>
2024-04-01 03:06:14 +0530

    Temporary feed env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,8 @@ import { ProjectsModule } from './modules/projects/projects.module';
       cache: true,
       isGlobal: true,
       validationSchema: Joi.object({
-        PORT: Joi.number().required(),
-        GLOBAL_ROUTING_PREFIX: Joi.string().required(),
+        PORT: Joi.number().optional().default(3000),
+        GLOBAL_ROUTING_PREFIX: Joi.string().optional().default('api'),
       }),
     }),
     TasksModule,
