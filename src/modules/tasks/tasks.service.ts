6531864638f31706169b1b2d6675c84372f9b279
ukswift <utkarshkousik@gmail.com>
2024-04-10 03:07:22 +0530

    Add types in tasks service

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -2,29 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TasksRepository } from './tasks.repository';
+import { TaskEntity } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
   constructor(private tasksRepository: TasksRepository) {}
-  async create(createTaskDto: CreateTaskDto) {
+  async create(createTaskDto: CreateTaskDto): Promise<TaskEntity> {
     return await this.tasksRepository.create({
       ...createTaskDto,
     });
   }
 
-  async findAll() {
+  async findAll(): Promise<TaskEntity[]> {
     return await this.tasksRepository.findAll();
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<TaskEntity> {
     return await this.tasksRepository.findOne(id);
   }
 
-  async update(id: string, updateTaskDto: UpdateTaskDto) {
+  async update(id: string, updateTaskDto: UpdateTaskDto): Promise<TaskEntity> {
     return await this.tasksRepository.updateOne(id, updateTaskDto);
   }
 
-  async delete(id: string) {
+  async delete(id: string): Promise<TaskEntity> {
     return await this.tasksRepository.deleteOne(id);
   }
 }
